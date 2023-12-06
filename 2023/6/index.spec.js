16a45ac5f8b3586c01bc3fad5ab4eb3b7bad30c3
oversizedhat <oscar@oscarberg.com>
2023-12-06 22:15:58 +0100

    skip bigger test

--- 2023/6/index.spec.js
+++ 2023/6/index.spec.js
@@ -11,12 +11,13 @@ test("input", async () => {
   expect(result).toBe(1155175);
 });
 
-test.only("extra example", async () => {
+test("extra example", async () => {
   const result = await extra.main("6/inputExample.txt");
   expect(result).toBe(71503);
 });
 
-test.only("extra input", async () => {
+// to slow
+test.skip("extra input", async () => {
   const result = await extra.main("6/input.txt");
   expect(result).toBe(35961505);
 });
